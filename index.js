7f79a4c9b04a9b2a35d13f3ddc136b85a8c57dbb
이원희 <wonhee_lee@tmax.co.kr>
2020-08-28 15:03:14 +0900

    register api 추가

--- index.js
+++ index.js
@@ -19,5 +19,21 @@ mongoose.connect('mongodb+srv://wonhee:dnjsdnjf12@boilerplate.7mhbn.mongodb.net/
 })
 .then(() => console.log('MongoDB connected!!'))
 .catch((err) => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello, World!'))
+
+app.post('/register', (req, res) => {
+    // 회원가입할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body);
+
+    // save: mongodb의 method
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    }); 
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
