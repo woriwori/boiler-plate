ea2e057c0d10874dead45aed937250f03722eca9
이원희 <wonhee_lee@tmax.co.kr>
2020-08-28 15:03:03 +0900

    body-parser 설치

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const {User} = require('./model/User')
+
+// application/x-www-form-urlencoded로 되어있는 데이터를 분석해서 가져올 수 있도록 해줌.
+app.use(bodyParser.urlencoded({extended: true})) 
+
+// application/json로 되어있는 데이터를 분석해서 가져올 수 있도록 해줌. 
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://wonhee:dnjsdnjf12@boilerplate.7mhbn.mongodb.net/boilerplate?retryWrites=true&w=majority', {
