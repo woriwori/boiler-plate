de5cdb0bbeaf97b3195fd923f34270629d8209e9
이원희 <wonhee_lee@tmax.co.kr>
2020-09-07 17:02:32 +0900

    proxy설정으로 cors이슈 해결해서 서버연동
    client : localhost:3000에서
    server : localhost:5000으로 문제없이 요청할 수 있도록함.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { auth } = require('./middleware/auth');
@@ -95,4 +94,9 @@ app.get('/api/users/logout', auth, (req, res) => {
     });
 });
 
+app.get('/api/hello', (req, res) => {
+    res.send('안녕하세여');
+});
+
+const port = 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
