6de3daab6da33ab6c83764b1906c9146d2cd4831
[3기]이원희 <ireba4478@gmail.com>
2020-10-07 22:45:26 +0900

    redux 초기 설정

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { applyMiddleware } from 'redux';
+
+// redux 사용에 도움을 주는 미들웨어
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById('root'),
 );
 
