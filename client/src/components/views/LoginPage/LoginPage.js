be684a1d37d642216cb97dca7c49aed3c3321103
[3기]이원희 <ireba4478@gmail.com>
2020-10-08 19:15:56 +0900

    login page에 id/pw 입력폼 추가

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useState } from 'react';
 
 function LoginPage() {
-    return <div>LoginPage</div>;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onEmailHandler = (e) => {
+        setEmail(e.currentTarget.value);
+    };
+    const onPasswordHandler = (e) => {
+        setPassword(e.currentTarget.value);
+    };
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        console.log('Email', email);
+        let body = {
+            email,
+            password,
+        };
+        Axios.post('/api/users/login', body).then((response) => {});
+    };
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
+            <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type="email" value={email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={password} onChange={onPasswordHandler} />
+                <br />
+                <button>Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default LoginPage;
