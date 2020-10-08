38f66d7c0eee4b1aad3717ca0d5cb0401638ed97
[3기]이원희 <ireba4478@gmail.com>
2020-10-08 23:18:11 +0900

    회원가입 구현

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,9 +1,11 @@
-import { LOGIN_USER } from '../_actions/types';
+import { LOGIN_USER, REGISTER_USER } from '../_actions/types';
 
 export default function (prevState = {}, action) {
     switch (action.type) {
         case LOGIN_USER:
             return { ...prevState, loginSuccess: action.payload };
+        case REGISTER_USER:
+            return { ...prevState, success: action.payload };
         default:
             return prevState;
     }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../../../_actions/user_action';
 
-function RegisterPage() {
-    return <div>RegisterPage</div>;
+function RegisterPage(props) {
+    const dispatch = useDispatch();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const onNameHandler = (e) => {
+        setName(e.currentTarget.value);
+    };
+    const onEmailHandler = (e) => {
+        setEmail(e.currentTarget.value);
+    };
+    const onPasswordHandler = (e) => {
+        setPassword(e.currentTarget.value);
+    };
+    const onConfirmPasswordHandler = (e) => {
+        setConfirmPassword(e.currentTarget.value);
+    };
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            return alert('비밀번호와 비밀번호 확인은 같아야 합니다.');
+        }
+
+        const body = {
+            name,
+            email,
+            password,
+        };
+        dispatch(registerUser(body)).then((response) => {
+            console.log(response);
+            if (response.payload.success) {
+                props.history.push('/'); // react에서 페이지 이동하는 방법
+            } else {
+                alert('error');
+            }
+        });
+    };
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                height: '100vh',
+            }}
+        >
+            <form
+                style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Name</label>
+                <input type="text" value={name} onChange={onNameHandler} />
+                <label>Email</label>
+                <input type="email" value={email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={onPasswordHandler}
+                />
+                <label>Confirm Password</label>
+                <input
+                    type="password"
+                    value={confirmPassword}
+                    onChange={onConfirmPasswordHandler}
+                />
+                <br />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
 }
 
 export default RegisterPage;
