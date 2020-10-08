f5446847fabbafc3797cccda5c0ff12613e348c1
[3기]이원희 <ireba4478@gmail.com>
2020-10-08 19:15:33 +0900

    landingpage에 css 수정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ function LandingPage() {
     useEffect(() => {
         axios.get('/api/hello').then((res) => console.log(res));
     }, []);
-    return <div>LandingPage</div>;
+    return <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>LandingPage</div>;
 }
 
 export default LandingPage;
